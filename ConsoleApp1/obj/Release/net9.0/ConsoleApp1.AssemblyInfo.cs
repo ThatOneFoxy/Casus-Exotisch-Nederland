2e76d4192e39cf34868122add2d209ef561d7719
Djody Kort <507946@vistacollege.nl>
2025-01-21 13:25:06 +0100

    d

--- ConsoleApp1/obj/Release/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Release/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a703d40cb7a13efe38bd1a47a4080f213de73f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3901e923bde94b7f0dbb24f284438ec89486c07")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
