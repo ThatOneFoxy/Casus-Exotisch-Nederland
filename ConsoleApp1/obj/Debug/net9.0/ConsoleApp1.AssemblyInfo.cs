be30fac9ff05d7859e72862da09b1b05ae42b4d5
Djody Kort <507946@vistacollege.nl>
2025-01-21 14:11:16 +0100

    NOT DONE - API NOT WORKING YET

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3901e923bde94b7f0dbb24f284438ec89486c07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0fbafe198a877012db921b6f0e9ea5a6240e419")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
