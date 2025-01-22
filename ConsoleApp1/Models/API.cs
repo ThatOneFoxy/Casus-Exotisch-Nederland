7db2a3ba90462682af79ec16ed67aa4acb4d4ae6
Djody Kort <507946@vistacollege.nl>
2025-01-22 16:41:59 +0100

    Basic API functionality

--- ConsoleApp1/Models/API.cs
+++ ConsoleApp1/Models/API.cs
@@ -1,5 +1,7 @@
 ﻿// ======== Imports ========
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1.Models;
@@ -14,10 +16,30 @@ public class API {
     }
 
     // ==== Methods ====
-    public async Task GetDataFromAPI() {
+    public async Task<List<string>?> GetDataFromAPI() {
         using (HttpClient client = new HttpClient()) {
             try {
                 HttpResponseMessage response = await client.GetAsync(this.apiURL);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var jsonData = JsonSerializer.Deserialize<List<string>>(responseBody);
+                return jsonData;
+            }
+            catch (HttpRequestException e) {
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+        }
+        return null;
+    }
+
+    public async Task PostDataToAPI(List<string> data) {
+        using (HttpClient client = new HttpClient()) {
+            try {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, this.apiURL);
+                request.Content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json");
+
+
+                HttpResponseMessage response = await client.SendAsync(request);
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
                 Console.WriteLine(responseBody);
@@ -25,9 +47,9 @@ public class API {
             catch (HttpRequestException e) {
                 Console.WriteLine($"An error occurred: {e.Message}");
             }
+
         }
     }
-
     // ==== Constructor ====
     public API(string apiURL) {
         this.apiURL = apiURL;
