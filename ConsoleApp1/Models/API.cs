b648b5e9d67745079ae03d1a3d667f8585eb7188
Djody Kort <507946@vistacollege.nl>
2025-01-22 16:21:16 +0100

    DONE - Added first API retrieval function

--- ConsoleApp1/Models/API.cs
+++ ConsoleApp1/Models/API.cs
@@ -1,5 +1,35 @@
-﻿namespace ConsoleApp1.Models;
+﻿// ======== Imports ========
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models;
 
 public class API {
-    
+    // ==== Properties ====.
+    private string? apiURL = "";
+
+    // ==== Getters ====
+    public string GetAPIURL() {
+        return this.apiURL;
+    }
+
+    // ==== Methods ====
+    public async Task GetDataFromAPI() {
+        using (HttpClient client = new HttpClient()) {
+            try {
+                HttpResponseMessage response = await client.GetAsync(this.apiURL);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e) {
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+        }
+    }
+
+    // ==== Constructor ====
+    public API(string apiURL) {
+        this.apiURL = apiURL;
+    }
 }
